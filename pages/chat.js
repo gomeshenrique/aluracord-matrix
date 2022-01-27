a72f4c9abce553a036d9892b06928396a41bebe5
Henrique Gomes <henriquegomes@outlook.com>
2022-01-27 20:42:39 -0300

    imersão alura aula4

--- pages/chat.js
+++ pages/chat.js
@@ -1,19 +1,44 @@
 import { Box, Text, TextField, Image, Button } from "@skynexui/components";
 import React from "react";
 import appConfig from "../config.json";
+import { createClient } from "@supabase/supabase-js";
+
+const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzMyMjUyOSwiZXhwIjoxOTU4ODk4NTI5fQ.FiLmKNXFBgvHX_YXjJyG1W4ERj9Pa0aYOWecwqNOL0I";
+const SUPABASE_URL = "https://wuzrlggveywvvscpwjky.supabase.co";
+const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export default function ChatPage() {
   // Sua lógica vai aqui
   const [mensagem, setMensagem] = React.useState("");
   const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
 
+  React.useEffect(() => {
+    supabaseClient
+      .from("mensagens")
+      .select("*")
+      .order("id", { ascending: false })
+      .then(({ data }) => {
+        console.log("Dados da consulta: ", data);
+        setListaDeMensagens(data);
+      });
+  }, []);
+
   function handleNovaMensagem(novaMensagem) {
     const mensagem = {
-      texto: novaMensagem,
+      // id: listaDeMensagens.length + 1,
       de: "gomeshenrique",
-      id: listaDeMensagens.length + 1,
+      texto: novaMensagem,
     };
-    setListaDeMensagens([mensagem, ...listaDeMensagens]);
+
+    supabaseClient
+      .from("mensagens")
+      .insert([mensagem])
+      .then(({ data }) => {
+        console.log("Criando mensagem: ", data);
+        setListaDeMensagens([data[0], ...listaDeMensagens]);
+      });
+
     setMensagem("");
   }
 
@@ -191,7 +216,7 @@ function MessageList(props) {
                   display: "inline-block",
                   marginRight: "8px",
                 }}
-                src={`https://github.com/gomeshenrique.png`}
+                src={`https://github.com/${mensagem.de}.png`}
               />
               <Text tag="strong">{mensagem.de}</Text>
               <Text
@@ -204,14 +229,14 @@ function MessageList(props) {
               >
                 {new Date().toLocaleDateString()}
               </Text>
-              <Button
+              {/* <Button
                 label="X"
-                // onClick={}
+                onClick={}
                 styleSheet={{
                   marginLeft: "auto",
                   borderRadius: "50%",
                 }}
-              />
+              /> */}
             </Box>
             {mensagem.texto}
           </Text>
